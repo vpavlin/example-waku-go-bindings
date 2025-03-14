e3f3cde8dc3b1e205c793dc3c8678a61988745df
Václav Pavlín <vaclav.pavlin@gmail.com>
2025-03-14 14:41:11 +0100

    remove dialer

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 	"time"
 
 	"github.com/multiformats/go-multiaddr"
-	"github.com/waku-org/go-waku/waku/v2/protocol/pb"
 	"github.com/waku-org/waku-go-bindings/waku"
 	"github.com/waku-org/waku-go-bindings/waku/common"
 	"go.uber.org/zap"
 )
+
+const BootstrapNode = "/dns4/waku-test.bloxy.one/tcp/30304/p2p/16Uiu2HAmSZbDB7CusdRhgkD81VssRjQV5ZH13FbzCGcdnbbh6VwZ"
 
 const AppName = "example"
 const AppVersion = "1"
@@ -32,28 +33,6 @@ func main() {
 
 	const requestTimeout = 30 * time.Second
 	// Configure dialer node
-	dialerNodeWakuConfig := common.WakuConfig{
-		Relay:           true,
-		LogLevel:        "DEBUG",
-		Discv5Discovery: false,
-		ClusterID:       42,
-		Shards:          []uint16{0},
-		Discv5UdpPort:   9020,
-		TcpPort:         60020,
-	}
-
-	// Create and start dialer node
-	dialerNode, err := waku.NewWakuNode(&dialerNodeWakuConfig, "dialerNode")
-	if err != nil {
-		fmt.Printf("Failed to create dialer node: %v\n", err)
-		return
-	}
-	if err := dialerNode.Start(); err != nil {
-		fmt.Printf("Failed to start dialer node: %v\n", err)
-		return
-	}
-	defer dialerNode.Stop()
-	time.Sleep(1 * time.Second)
 
 	// Configure receiver node
 	receiverNodeWakuConfig := common.WakuConfig{
@@ -86,37 +65,12 @@ func main() {
 		return
 	}
 
-	// Check initial peer counts
-	dialerPeerCount, err := dialerNode.GetNumConnectedPeers()
-	if err != nil {
-		fmt.Printf("Failed to get dialer peer count: %v\n", err)
-		return
-	}
-	fmt.Printf("Dialer initial peer count: %d\n", dialerPeerCount)
-
 	receiverPeerCount, err := receiverNode.GetNumConnectedPeers()
 	if err != nil {
 		fmt.Printf("Failed to get receiver peer count: %v\n", err)
 		return
 	}
 	fmt.Printf("Receiver initial peer count: %d\n", receiverPeerCount)
-
-	// Dial peer
-	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
-	defer cancel()
-	if err := dialerNode.Connect(ctx, receiverMultiaddr[0]); err != nil {
-		fmt.Printf("Failed to dial peer: %v\n", err)
-		return
-	}
-	time.Sleep(1 * time.Second)
-
-	// Check final peer counts
-	dialerPeerCount, err = dialerNode.GetNumConnectedPeers()
-	if err != nil {
-		fmt.Printf("Failed to get dialer peer count: %v\n", err)
-		return
-	}
-	fmt.Printf("Dialer final peer count: %d\n", dialerPeerCount)
 
 	receiverPeerCount, err = receiverNode.GetNumConnectedPeers()
 	if err != nil {
@@ -125,10 +79,13 @@ func main() {
 	}
 	fmt.Printf("Receiver final peer count: %d\n", receiverPeerCount)
 
-	publicNode, err := multiaddr.NewMultiaddr("/dns4/waku-test.bloxy.one/tcp/30304/p2p/16Uiu2HAmSZbDB7CusdRhgkD81VssRjQV5ZH13FbzCGcdnbbh6VwZ")
+	publicNode, err := multiaddr.NewMultiaddr(BootstrapNode)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
 	err = receiverNode.Connect(ctx, publicNode)
 	if err != nil {
@@ -142,16 +99,6 @@ func main() {
 			}
 		}
 	}()
-
-	for i := 0; i < 10; i++ {
-		payload := fmt.Sprintf("Message %d", i)
-		ctx := context.Background()
-		msg := &pb.WakuMessage{
-			Payload:      []byte(payload),
-			ContentTopic: ContentTopic,
-		}
-		dialerNode.RelayPublish(ctx, msg, "/waku/2/rs/42/0")
-	}
 
 	fmt.Println(receiverMultiaddr)
 
